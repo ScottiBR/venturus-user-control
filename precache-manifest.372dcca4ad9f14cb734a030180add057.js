fe8e2eee9b1e626b5f9a891559302635f6f98fe7
ScottiBR <guilhermescotti@gmail.com>
2019-07-11 12:18:29 -0300

    Updates

---
+++ precache-manifest.372dcca4ad9f14cb734a030180add057.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24c12be21680c0359d657afccabeab8d",
+    "url": "/venturus-user-control/index.html"
+  },
+  {
+    "revision": "9693dd56b760e79eea29",
+    "url": "/venturus-user-control/static/css/main.aedc4706.chunk.css"
+  },
+  {
+    "revision": "adb0514933913783199c",
+    "url": "/venturus-user-control/static/js/2.c5e891df.chunk.js"
+  },
+  {
+    "revision": "9693dd56b760e79eea29",
+    "url": "/venturus-user-control/static/js/main.2178b863.chunk.js"
+  },
+  {
+    "revision": "6bd01c2234b7eaf2a615",
+    "url": "/venturus-user-control/static/js/runtime~main.00902820.js"
+  }
+]);
